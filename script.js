440ba9796ae38543e30de76f063db5f832c96cc7
Anton <antonzhygalov@gmail.com>
2017-08-20 19:30:13 +0300

    Finished with Test

--- script.js
+++ script.js
@@ -5,31 +5,30 @@ const crypto = require('crypto');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-
-
 app.post('/users', function(req, res) {
     fs.readFile('storage.json', function(err, data) {
         if (err) console.log(err);
 
         var myData;
-        var id;
+        var id = 0;
+        var exist = false;
 
         if (data.length === 0) {
             myData = [];
         } else {
             myData = JSON.parse(data);
-            var exist = false;
+            for (var i = 0; i < myData.length; i++) {
+                if (myData[i].email === req.body.email) {
+                    exist = true;
+                    res.status(409).send('User with this email already exists');
+                    break;
+                }
+                id = myData[i].id;
+            }
         }
 
-        for (var i = 0; i < myData.length; i++) {
-            if (myData[i].email === req.body.email) {
-                exist = true;
-                res.status(409).send('User with this email already exists');
-                break;
-            }
-            id = myData[i].id;
-        }
-        if (!exist) {
+
+        if (!exist && req.body.username) {
             req.body.id = id + 1;
             req.body.password = crypto.createHmac('sha256', req.body.password).digest('hex');
             myData.push(req.body);
@@ -37,7 +36,7 @@ app.post('/users', function(req, res) {
                 if (err) console.log(err);
                 res.status(201).send('all good');
             })
-        }
+        } else res.status(400).send();
     });
 })
 
@@ -49,13 +48,14 @@ app.get('/users', function(req, res) {
             console.log(err);
             return;
         }
+        var myData = ['[]'];
         if (data.length >= 1) {
-            var myData = JSON.parse(data);
+            myData = JSON.parse(data);
             for (var i = 0; i < myData.length; i++) {
                 delete myData[i].password;
             }
             res.status(200).send(myData);
-        } else res.status(200).send();
+        } else res.status(200).send(myData);
 
     });
 })
@@ -88,7 +88,7 @@ app.get('/users/:id', function(req, res) {
 app.put('/users/:id', function(req, res) {
     fs.readFile('storage.json', function(err, data) {
         if (err) console.log(err);
-            
+
         if (data.length === 0) {
             res.status(404).send('Not found');
         } else {
@@ -102,11 +102,11 @@ app.put('/users/:id', function(req, res) {
                 myData[i].password = crypto.createHmac('sha256', req.body.password).digest('hex');
                 fs.writeFile('storage.json', JSON.stringify(myData, null, 2), function(err) {
                     if (err) console.log(err);
-                    
+
                 })
                 user = myData[i];
                 delete user.password;
-                res.status(200).send(user);  
+                res.status(200).send(user);
                 break;
             }
         }
@@ -120,7 +120,7 @@ app.put('/users/:id', function(req, res) {
 app.delete('/users/:id', function(req, res) {
     fs.readFile('storage.json', function(err, data) {
         if (err) console.log(err);
-            
+
         if (data.length === 0) {
             res.status(404).send('Not found');
         } else {
@@ -129,24 +129,21 @@ app.delete('/users/:id', function(req, res) {
         }
         for (var i = 0; i < myData.length; i++) {
             if (myData[i].id === parseInt(req.params.id)) {
-               	myData.splice(i, 1);
+                myData.splice(i, 1);
                 fs.writeFile('storage.json', JSON.stringify(myData, null, 2), function(err) {
                     if (err) console.log(err);
-                    
                 })
                 user = true;
-                
-                res.status(200).send('User has been successfully removed.');  
+
+                res.status(200).send({"message": "User has been successfully removed."});
                 break;
             }
         }
         if (!user) {
             res.status(404).send('Not found');
         }
-
     });
 })
-
 
 app.listen(3000, function() {
     console.log('Start server on port 3000!')
